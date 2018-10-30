c2cf3e2747225cf501c957ff70a32e7db1b507e4
Chris Sevilleja <chris@scotch.io>
2018-10-30 10:18:55 -0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function MyMessage({ message }: { message: string }) {
+  return <div>i shall speak! my message is: {message}</div>;
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+
+          <MyMessage message="hello" />
+
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
